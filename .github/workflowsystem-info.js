08356485eb45a968e3587f93521fde817826bbf0
Manish Sharma <itzmanish169@gmail.com>
2026-03-03 12:20:43 -0500

    Create workflowsystem-info.js

---
+++ .github/workflowsystem-info.js
@@ -0,0 +1,4 @@
+console.log("Operating System:", process.platform);
+console.log("Node Version:", process.version);
+console.log("CPU Architecture:", process.arch);
+console.log("Memory Usage:", process.memoryUsage());
